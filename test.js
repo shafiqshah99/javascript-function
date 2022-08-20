5ed0a9bddd97e2a3f25116b19a5ffdfa1ef299ce
shafiqshah99 <82441174+shafiqshah99@users.noreply.github.com>
2022-08-20 15:56:09 +0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,160 @@
+function reverse_a_number(n)
+{
+    n = n + "";
+    return n.split("").reverse().join("");
+}
+console.log(Number(reverse_a_number(32243)));
+
+function sub(str1)
+{
+var array1 = [];
+  for (var x = 0, y=1; x < str1.length; x++,y++) 
+  {
+   array1[x]=str1.substring(x, y);
+    }
+var combi = [];
+var temp= "";
+var slent = Math.pow(2, array1.length);
+
+for (var i = 0; i < slent ; i++)
+{
+    temp= "";
+    for (var j=0;j<array1.length;j++) {
+        if ((i & Math.pow(2,j))){ 
+            temp += array1[j];
+        }
+    }
+    if (temp !== "")
+    {
+        combi.push(temp);
+    }
+}
+  console.log(combi.join("\n"));
+}
+
+sub("dog");
+
+function alphabet_order(str)
+  {
+return str.split('').sort().join('');
+  }
+console.log(alphabet_order("webmaster"));
+
+
+function uppercase(str)
+{
+  var array1 = str.split(' ');
+  var newarray1 = [];
+    
+  for(var x = 0; x < array1.length; x++){
+      newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
+  }
+  return newarray1.join(' ');
+}
+console.log(uppercase("the quick brown fox"));
+
+
+function find_longest_word(str)
+{
+  var array1 = str.match(/\w[a-z]{0,}/gi);
+  var result = array1[0];
+
+  for(var x = 1 ; x < array1.length ; x++)
+  {
+    if(result.length < array1[x].length)
+    {
+    result = array1[x];
+    } 
+  }
+  return result;
+}
+console.log(find_longest_word('Web Development Tutorial'));
+
+
+function vowel_count(str1)
+{
+  var vowel_list = 'aeiouAEIOU';
+  var vcount = 0;
+  
+  for(var x = 0; x < str1.length ; x++)
+  {
+    if (vowel_list.indexOf(str1[x]) !== -1)
+    {
+      vcount += 1;
+    }
+  
+  }
+  return vcount;
+}
+console.log(vowel_count("The quick brown fox"));
+
+
+function test_prime(n)
+{
+
+  if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;  
+  }
+}
+
+console.log(test_prime(30));
+
+
+function detect_data_type(value)
+{
+var dtypes = [Function, RegExp, Number, String, Boolean, Object], x, len;
+    
+if (typeof value === "object" || typeof value === "function") 
+    {
+     for (x = 0, len = dtypes.length; x < len; x++) 
+     {
+            if (value instanceof dtypes[x])
+            {
+                return dtypes[x];
+            }
+      }
+    }
+    
+    return typeof value;
+}
+console.log(detect_data_type(12));
+console.log(detect_data_type('string'));
+console.log(detect_data_type(false));
+
+
+function Second_Greatest_Lowest(arr_num)
+{
+  arr_num.sort(function(x,y)
+           {
+           return x-y;
+           });
+  var uniqa = [arr_num[0]];
+  var result = [];
+  
+  for(var j=1; j < arr_num.length; j++)
+  {
+    if(arr_num[j-1] !== arr_num[j])
+    {
+      uniqa.push(arr_num[j]);
+    }
+  }
+    result.push(uniqa[1],uniqa[uniqa.length-2]);
+  return result.join(',');
+  }
+
+console.log(Second_Greatest_Lowest([1,2,3,4,5]));
